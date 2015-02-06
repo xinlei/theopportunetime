311e631167304f59a331398d92c2e4d89df7ea45
xinlei <wang.xinlei15@gmail.com>
2015-02-05 19:46:51 -0800

    first commit

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>The Opportune Time</title>
+	<link href='http://fonts.googleapis.com/css?family=Droid+Serif' rel='stylesheet' type='text/css'>
+	<link href="http://fonts.googleapis.com/css?family=Droid+Sans" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+	<link href='css/default.css' rel='stylesheet' type='text/css'>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+	<div class='header'>
+		<ul class='nav-bar'>
+			<li class='title'><h2>The Opportune Time</h2></li>	
+			<li class='item'><h5>Dictionary<h5></li>
+			<li class='item'><h5>Focus Areas<h5></li>
+			<li class='item'><h5>Subscribe<h5></li> 
+			<li class='item'><h5>Login/Signup<h5></li>
+			<li class='item'><h5>Writers<h5></li>
+			<li class='filler'><h5><h5></li>
+		</ul>
+	</div>
+	<div class='container'>
+		<div class='row'>
+			<div class='col-md-8 main-content title'><h1>Weekly Market Update</h1></div>
+		</div>
+		<div class='row'>
+			<div class='col-md-8 main-content img'><img src='img/Brent-Cartoon-1.jpg'></div>
+		</div>
+		<div class='row'>
+			<div class='col-md-8 main-content body'>
+				<p>Lorem ipsum dolor sit amet, et scelerisque etiam turpis sed imperdiet eget, sit nec vitae ut risus. Mauris ac aliquam aliquam metus felis netus. Habitasse risus nullam eget nec, sit mattis ipsum mattis vel magna, congue tempus sed justo voluptas, at a, eleifend metus id at. Massa nostra eros at feugiat. Duis ornare ac eu tellus lectus dui, volutpat ac, iaculis iaculis, sollicitudin taciti amet integer et aliquam. Pellentesque aliquet scelerisque duis diam at. In urna sed lacinia porttitor enim, lacus sed non elementum rutrum, quam aptent, dictum consequat rhoncus per massa ante ligula. Dolor mi massa ante.</p>
+				<p>Quis quis cum inceptos tellus, netus dui, tellus a ipsum vehicula, mauris risus libero ut. Ut sed pharetra nonummy amet praesent, ultricies consequat ut eu non proin. Morbi nec mi. Mauris augue, sit imperdiet purus aliquet et, nunc molestie, purus vel metus lectus metus, tempor elementum pulvinar donec quis scelerisque. Adipiscing primis nec, nec nonummy id et magna ac. Adipiscing vitae quisquam aenean risus eget, vehicula sed libero sem aliquam massa elit. Aliquet adipiscing nonummy ut vel, quisque vel.</p>
+			</div>
+		</div>
+		<div class='row'><div class='col-md-8 content-divider'></div></div>
+		<div class='row'>
+			<div class='col-md-8 sub-content-title'><h3>Mixed Feelings: The Causes and Effects of Falling Oil Prices</h3></div>
+		</div>
+		<div class='row'>
+			<div class='col-md-8 sub-content-sub-title'><h5>With oil prices tumbling, OPEC is not the only entity struggling to remain on top of their finances.</h5></div>
+		</div>
+		<div class='row'>
+			<div class='col-md-8 sub-content-author'><h5>By Andrew Vazquez, Managing Editor</h5></div>
+		</div>
+		<div class='row'><div class='col-md-8 content-divider'></div></div>
+		<div class='row'>
+			<div class='col-md-8 sub-content-title'><h3>Reversal of the Dollar Carry Trade set to Shake up the World</h3></div>
+		</div>
+		<div class='row'>
+			<div class='col-md-8 sub-content-sub-title'><h5>The end of U.S. quantitative easing could be the turning point down a very dark road for emerging market economies.</h5></div>
+		</div>
+		<div class='row'>
+			<div class='col-md-8 sub-content-author'><h5>By Andrew Vazquez, Managing Editor</h5></div>
+		</div>
+		<div class='row'><div class='col-md-8 content-divider'></div></div>
+
+	</div>
+</body>
+
+</html>
